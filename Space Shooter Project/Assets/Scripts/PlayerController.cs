c17eab5d2c7b49b609f674bb059378f14e7e5ffe
Ivan <S1lveria@users.noreply.github.com>
2017-12-21 00:02:07 -0500

    Update the PlayerController Script
    - Set boundaries for the movements of ship within the map
    - Serialize the Boundary class
    - Optimize the code
    - Create quaternion for x-axis

--- Space Shooter Project/Assets/Scripts/PlayerController.cs
+++ Space Shooter Project/Assets/Scripts/PlayerController.cs
@@ -2,16 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
 public class PlayerController : MonoBehaviour
 {
 	public float speed;
+	public float tilt;
+	public Boundary boundary;
 
     void FixedUpdate()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 position = GetComponent<Rigidbody>().position;
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		GetComponent<Rigidbody>().velocity = movement * speed;
+		GetComponent<Rigidbody> ().position = new Vector3 
+		(
+			Mathf.Clamp(position.x, boundary.xMin, boundary.xMax), 0.0f, 
+			Mathf.Clamp(position.z, boundary.zMin, boundary.zMax)
+		);
+
+		GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
     }
 }
